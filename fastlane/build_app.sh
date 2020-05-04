e33306c9cc4af9f03835effb13450f1a4b412048
Samir <samir.bhide@id123.io>
2020-05-04 09:15:17 +0530

    added copy of fastlane files from id123

---
+++ fastlane/build_app.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+echo "----------------------- build_app.sh started-----------------------------"
+echo $1
+echo $2
+echo $3
+
+echo "-----------------BUILD STARTED--------------------"
+xcodebuild -UseModernBuildSystem=NO -scheme "Alumni ID" -workspace "Idee App.xcworkspace" -configuration $1 build
+echo "-----------------BUILD ENDED--------------------"
+
+echo "-----------------ARCHIVE STARTED--------------------"
+xcodebuild -UseModernBuildSystem=NO -scheme "Alumni ID" -workspace "Idee App.xcworkspace" -archivePath "./fastlane/app.xcarchive" -configuration $1 PROVISIONING_PROFILE="~/Library/MobileDevice/Provisioning Profiles/$2.mobileprovision" archive
+echo "-----------------ARCHIVE ENDED--------------------"
+
+echo "-----------------EXPORT STARTED--------------------"
+xcrun xcodebuild -exportArchive -exportOptionsPlist "./exportOptions.plist" -archivePath "./fastlane/app.xcarchive" -exportPath "./fastlane/IPA"
+echo "-----------------EXPORT ENDED--------------------"
---
+++ fastlane/exportoptions.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+/usr/libexec/PlistBuddy -c ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :method string "ad-hoc"" ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :teamID string "$1"" ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :compileBitcode bool false" ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :uploadBitcode bool false" ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :signingCertificate string "$2"" ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :signingStyle string "manual"" ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :provisioningProfiles:$3 string $4" ./exportOptions.plist
+/usr/libexec/PlistBuddy -c "Add :provisioningProfiles:$5 string $6" ./exportOptions.plist
