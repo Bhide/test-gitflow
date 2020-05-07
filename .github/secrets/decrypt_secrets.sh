2ae3a12e07408ba2f05e330535914cd1f6507c4f
Samir <samir.bhide@id123.io>
2020-05-07 09:20:27 +0530

    changes to fastfile, scripts and workflows

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -3,10 +3,12 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/83d1fbcd-7c19-4612-b1a2-329963ba4273.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/iPhone\ Distribution.p12.gpg
 
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile_id_card_widget.mobileprovision ./.github/secrets/034a0e6f-37db-42c8-9f60-fe2c92b7af2a.mobileprovision.gpg
+
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/83d1fbcd-7c19-4612-b1a2-329963ba4273.mobileprovision
-
+cp ./.github/secrets/profile_id_card_widget.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/034a0e6f-37db-42c8-9f60-fe2c92b7af2a.mobileprovision
 
 echo "CREATE NEW KEYCHAIN AND COPY CERTIFICATE TO IT. UNLOCK THE KEYCHAIN TO MAKE IT ACCESSIBLE"
 
